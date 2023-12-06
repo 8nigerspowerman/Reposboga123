6a3a9002effb7c21f6e8487f4fdc4418c1e21360
8nigerspowerman <149361753+8nigerspowerman@users.noreply.github.com>
2023-12-06 18:37:08 +0800

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "Point.h"
+#include "line.h"
+
+using namespace std;
+
+int main()
+{
+    Point p1 = Point(1);
+    Point p2 = Point(4, 2);
+
+    line line = line(p1, p2);
+    cout << line;
+}
